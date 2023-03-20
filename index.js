a5f2ca0463846ca9a09a54a1da183cdaf903104b
sheida noori <sheida.nouri@gmail.com>
2023-03-20 22:22:51 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(names, eventName){
+    const newArray=[...names];
+    for(let i= 0; i<names.length ;i++){
+    
+        newArray[i] =        `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+
+    }
+    return newArray
+}
+//writeCards(names, eventName) ;
+
+function countDown (num){
+    while(num>=0){
+        console.log(num)
+        num--
+    }
+}
